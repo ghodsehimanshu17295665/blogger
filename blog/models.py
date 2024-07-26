a18ea6e208d4d2f3c088b75a92d6415578f736e2
mansi <mnayak@bestpeers.com>
2024-07-26 17:34:08 +0530

    created post, category model and migration file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
+import uuid
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+
+
+
+
+
+
