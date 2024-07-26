db9b023076ed2aebe8afb687a17f74b2185c757c
mansi <mnayak@bestpeers.com>
2024-07-26 19:14:07 +0530

    created post, category models

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,45 @@ from django.utils import timezone
 from django.db import models
 
 
-class Post(models.Model):
+class TimesStampedModel(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+    
+
+class Post(TimesStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+# category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
 
 
-class Category(models.Model):
+class Category(TimesStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=25)
     description = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 
 
