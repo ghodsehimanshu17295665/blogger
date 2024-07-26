531a1db5f9f3c51d6df0853d5398294bed00f681
mansi <mnayak@bestpeers.com>
2024-07-26 19:11:15 +0530

    Registered models in admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from .models import Post, Category
 
 
 # Register your models here.
-from .models import Post, Category
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content')
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+
+
+
