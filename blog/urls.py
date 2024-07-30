c0f06cba61cf1b0ef49d61a6d1fe011593c36b51
Himanshu <himanshughodse@gmail.com>
2024-07-30 10:46:26 +0530

    Created urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 # from django.contrib import admin
 from django.urls import path
 from .views import home
+from .views import post_list
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", home),
-]
+    path('post_list/', post_list, name='post_list'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
