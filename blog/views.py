b9523af507ad378f68521b92a7ca5bc5358773ed
Himanshu <himanshughodse@gmail.com>
2024-07-30 10:46:01 +0530

    Created urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
