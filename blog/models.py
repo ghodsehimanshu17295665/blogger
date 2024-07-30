df57150c1d69279ec3d8577f371750ebe6c735e1
Himanshu <himanshughodse@gmail.com>
2024-07-30 10:47:59 +0530

    Created Post, Category, Comment Models

--- blog/models.py
+++ blog/models.py
@@ -11,16 +11,33 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class Post(TimeStampedModel):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=30)
-    content = models.TextField()
-
-
 class Category(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=25)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Post(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='posts', null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='posts', null=True)
+    author_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.author_name
