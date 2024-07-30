bb82dd1a584fde4945bb1bdd16e1757af19ca793
Himanshu <himanshughodse@gmail.com>
2024-07-30 10:49:16 +0530

    Registered models in admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content')
+    list_display = ('id', 'title', 'content', 'image')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'post', 'author_name')
