cf9fb5d91214e72c0b062acac7f61af606942c36
Himanshu <himanshughodse@gmail.com>
2024-08-01 17:42:33 +0530

    Created urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
-# from django.contrib import admin
 from django.urls import path
-from .views import home
-from .views import post_list
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import read_post
+from .views import (home, create_post, post_list,
+                    delete_post, update_post)
 
 urlpatterns = [
     path("", home),
-    path('post_list/', post_list, name='post_list'),
+    path('post/list/', post_list, name='post_list'),
+    path('create/post/', create_post, name='create_post'),
+    path('delete/post/<uuid:pk>/', delete_post, name='delete_post'),
+    path('update/post/<uuid:pk>/', update_post, name='update_post'),
+    path('view/post/<uuid:pk>/', read_post, name='read_post'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
