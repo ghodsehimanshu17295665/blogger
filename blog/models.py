a678113b904316ceb7a08098140c4245372b98a3
mansi <mnayak@bestpeers.com>
2024-08-01 17:36:04 +0530

    models of blog

--- blog/models.py
+++ blog/models.py
@@ -34,8 +34,8 @@ class Post(TimesStampedModel):
 
 class Comment(TimesStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, 
-                                 related_name='posts', null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='posts', null=True)
     text = models.TextField()
 
     def __str__(self):
