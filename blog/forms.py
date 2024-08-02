d29f0c5339f6f8e0593b84ffeba129c645eff73d
mansi <mnayak@bestpeers.com>
2024-08-02 13:57:41 +0530

    form validation

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,10 @@ class PostForm(forms.ModelForm):
                 'placeholder': 'Enter the Content'
                 })
         }
+    
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        word_count = len(content.split())
+        if word_count > 500:
+            raise forms.ValidationError('content should not exceed more than 500 words. It currently have {} words.'.format(word_count))
+        return content
