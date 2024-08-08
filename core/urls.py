7b01b29300314b7d8cf5d12c27839fb2ec462d37
mansi <mnayak@bestpeers.com>
2024-08-08 12:17:39 +0530

    urls of the core project

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('profile/', views.user_profile, name='profile'),
+    path('logout/', views.user_logout, name='logout'),
+
+
 ]
