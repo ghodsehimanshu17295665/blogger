8311d77a5dbd44f320e6945f86a4e09751300c40
mansi <mnayak@bestpeers.com>
2024-08-08 12:15:39 +0530

    url of the blog application

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path
-from blog.views import home
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import all_posts, create_post, update_post, view_post, delete_post
-
+from .views import Home, AllPosts, CreatePost, UpdatePost, ViewPost, DeletePost
 
 urlpatterns = [
-    path("", home),
-    path('all/posts/', all_posts, name='all_posts'),
-    path('create/post/', create_post, name='create_post'),
-    path('update/post/<uuid:pk>/', update_post, name='update_post'),
-    path('view/post/<uuid:pk>/', view_post, name='view_post'),
-    path('delete/post/<uuid:pk>/', delete_post, name='delete_post'),
+
+      path('', Home.as_view(), name='home'),
+      path('all/posts/', AllPosts.as_view(), name='allposts'),
+      path('create/post/', CreatePost.as_view(), name='create_post'),
+      path('update/post/<uuid:pk>', UpdatePost.as_view(), name='update_post'),
+      path('view/post/<uuid:pk>', ViewPost.as_view(), name='view_post'),
+      path('delete/post/<uuid:pk>', DeletePost.as_view(), name='delete_post'),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 
 
