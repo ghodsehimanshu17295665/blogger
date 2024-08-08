0222798a30fd187be25ffd78ed1d67e065ab2961
mansi <mnayak@bestpeers.com>
2024-08-08 12:16:27 +0530

    views of the blog application

--- blog/views.py
+++ blog/views.py
@@ -1,56 +1,152 @@
-from django.shortcuts import render, redirect
-from .models import Post
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from blog.forms import PostForm
+from django.views.generic import TemplateView
+from blog.models import Post
+# from django.http import HttpResponse
+from .forms import SignUpForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
-def home(request):
-    return render(request, "index.html")
+class Home(TemplateView):
+    template_name = "index.html"
 
+    def get(self, request):
+        return render(request, self.template_name)
+    
 
-def all_posts(request):
-    posts = Post.objects.all()
-    return render(request, 'crud/all_posts.html', {'posts': posts})
+class AllPosts(TemplateView):
+    template_name = 'crud/all_posts.html'
 
+    def get(self, request):
 
-def create_post(request):
-    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('/login/')
+        
+        posts = Post.objects.all()
+        context = {
+            "posts": posts
+        }
+        return render(request, self.template_name, context=context)
+    
+
+class CreatePost(TemplateView):
+    template_name = 'crud/create_post.html'
+
+    def get(self, request):
+        form = PostForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.info(request, 'Post Created Successfully!!')
-    else:
-        form = PostForm()
+            return redirect("create_post")
+        else:
+            form = PostForm()
+        return render(request, self.template_name, {"form": form})
 
-    return render(request, 'crud/create_post.html', {'form': form})
 
+class UpdatePost(TemplateView):
+    template_name = 'crud/update_post.html'
 
-def update_post(request, pk):
-    post = Post.objects.filter(id=pk).first()
-    if request.method == 'POST':
+    def get(self, request, pk):
+        post = Post.objects.filter(id=pk).first()
         if post:
-            data = request.POST
-            form = PostForm(data, instance=post)
+            form = PostForm(instance=post)
+            return render(request, self.template_name, {'form': form})
+        # else:
+        #     # return HttpResponse("Invalid Post ID....")
+
+    def post(self, request, pk):
+        post = Post.objects.filter(id=pk).first()
+        if post:
+            form = PostForm(request.POST, instance=post)
             if form.is_valid():
                 form.save()
-        else:
-            return messages.info(request, 'Invalid Post ID!!')
+                return render(request, self.template_name, {'form': form})
+            else:
+                return render(request, self.template_name, {'form': form})
+        
 
+class ViewPost(TemplateView):
+    template_name = 'crud/view_post.html'
+
+    def get(self, request, pk):
+        post = Post.objects.filter(id=pk).first()
+        if post:
+            return render(request, self.template_name, {"post": post})
+        # else:
+        #     # return HttpResponse("Post not found.")
+
+
+class DeletePost(TemplateView):
+    template_name = 'crud/all_post.html'
+
+    def post(self, request, pk):
+        post = Post.objects.get(id=pk)
+        if post:
+            post.delete()
+            return redirect('allposts')
+        return render(request, self.template_name, {'post': post})
+
+# SIGNUP
+
+
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Account Created Successfully!!")
+            form.save()
     else:
-        form = PostForm(instance=post)
-
-    return render(request, 'crud/update_post.html', {'form': form})
-
-
-def view_post(request, pk):
-    post = Post.objects.filter(id=pk).first()
-    return render(request, 'crud/view_post.html', {'post': post})
+        form = SignUpForm()
+    return render(request, 'enroll/signup.html', {'form': form})
+    
+# Login
 
 
-def delete_post(request, pk):
-    post = Post.objects.filter(id=pk)
-    if request.method == 'POST':
-        post.delete()
-        messages.info(request, 'Post Deleted Successfully!!')
-        return redirect('/all/posts/')
-    return render(request, 'crud/all_posts.html', {'post': post})
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                user_name = form.cleaned_data['username']
+                user_pass = form.cleaned_data['password']
+                user = authenticate(username=user_name, password=user_pass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in Successfully')
+                    return redirect('/profile/')
+                else:
+                    form.add_error(None, 'Invalid username or password')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html', {'form': form})
+    else:
+        return HttpResponseRedirect('/profile/')
+
+
+# profile
+
+
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'enroll/profile.html', {'name': request.user})
+    else:
+        return HttpResponseRedirect('/login/')
+
+# logout
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+
+ 
+
