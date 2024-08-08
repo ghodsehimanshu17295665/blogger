11da3aa9ee21357caf4d15aba779f7ae22390a5f
mansi <mnayak@bestpeers.com>
2024-08-08 12:06:02 +0530

    signup form is created

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class PostForm(forms.ModelForm):
@@ -7,8 +9,13 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'category']
 
-        widgets = {
-            "content": forms.Textarea(attrs={
-                'placeholder': 'Enter the Content'
-                })
-        }
+        
+class SignUpForm(UserCreationForm):
+    password2 = forms.CharField(label='Confirm Password (again)', 
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+        labels = {'email': 'Email'}
+
